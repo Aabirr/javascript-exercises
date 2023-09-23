c1bf64dd0ca4678302fcb4bbcf7d49f83ae51e68
Aabirr <aabirkashif@gmail.com>
2023-09-24 04:20:43 +0600

    updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+     const array = args[0];
+     const myArr = [];
 
+     array.forEach(element => {
+        if(!args.includes(element)) {
+            myArr.push(element);
+        }
+     });
+     return myArr;
 };
 
+removeFromArray([1,2,3,4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
